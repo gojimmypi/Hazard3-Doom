69462f11c76ebb2eb6631c393492ba4e7c0bbccd
gojimmypi <gojimmypi@gmail.com>
2026-07-25 17:26:04 -0700

    Disable VGND, VDPWR

--- src/project.v
+++ src/project.v
@@ -6,9 +6,11 @@
 `default_nettype none
 
 module tt_um_example (
+`ifdef OTHER_SHUTTLKE
     input  wire       VGND,
     input  wire       VDPWR,    // 3.3v core power supply
-//    input  wire       VAPWR,    // second analog power supply (VAA)
+//  input  wire       VAPWR,    // second analog power supply (VAA)
+`endif
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
