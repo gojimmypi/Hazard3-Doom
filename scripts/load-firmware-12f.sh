5bdbd471f6808386482343a0a87026277f18fe9e
gojimmypi <gojimmypi@gmail.com>
2026-08-24 14:37:27 -0700

    Detail file loaded

--- scripts/load-firmware-12f.sh
+++ scripts/load-firmware-12f.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd -- "${SCRIPT_DIR}/.." && pwd)"
 ELF="${1:-${ROOT_DIR}/build/ulx3s-12f/monitor/hazard3-boot-monitor.elf}"
-
+echo "Loading: "
+ls -al ${ELF}
 exec "${SCRIPT_DIR}/load-firmware.sh" "${ELF}"
